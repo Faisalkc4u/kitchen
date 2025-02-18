cca8b87682e3c12eedb18b1df24014d9b53792e4
Faisal <faisal@mvp-apps.ae>
2025-02-18 23:46:06 +0400

    feat: add support for additional image domain and replace img with next/image in banner swiper

--- src/components/banner/banner-swiper.tsx
+++ src/components/banner/banner-swiper.tsx
@@ -5,6 +5,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css";
 import "./banner-swiper.css";
+import Image from "next/image";
 export const BannerSwiper = () => {
   return (
     <div className="banner-swiper">
@@ -16,13 +17,19 @@ export const BannerSwiper = () => {
         onSwiper={(swiper) => console.log(swiper)}
       >
         <SwiperSlide>
-          <img
+          <Image
+            width={1920}
+            height={1080}
+            style={{ width: "100%", height: "100%" }}
             src="https://f.nooncdn.com/mpcms/EN0001/assets/e46dae7c-2d55-40f4-9e80-84baf6eb4c45.png?format=avif"
             alt=""
           />
         </SwiperSlide>
         <SwiperSlide>
-          <img
+          <Image
+            width={1920}
+            height={1080}
+            style={{ width: "100%", height: "100%" }}
             src="https://f.nooncdn.com/mpcms/EN0001/assets/feb896ca-4728-4f21-94e7-d2d83c3195f0.gif?format=avif"
             alt=""
           />
